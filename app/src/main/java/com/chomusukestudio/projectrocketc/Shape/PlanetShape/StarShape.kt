3e2e4a7f15d40c2aa135340823bf83a3c3020ff2
chomosuke <chomosuke@users.noreply.github.com>
2018-12-17 16:02:53 +1300

    Revert "Merge pull request #1 from chomosuke/UpgradeTrace"
    This reverts commit 1ec2753e28a94194cb3b4b85a15825bda9537139, reversing
    changes made to c35b43a075203a2508d09bba61d2b7daf7687e9e.

--- app/src/main/java/com/chomusukestudio/projectrocketc/Shape/PlanetShape/StarShape.kt
+++ app/src/main/java/com/chomusukestudio/projectrocketc/Shape/PlanetShape/StarShape.kt
@@ -6,7 +6,7 @@ import com.chomusukestudio.projectrocketc.Shape.Shape
 import java.lang.Math.random
 
 class StarShape(centerX: Float, centerY: Float, brightness: Float, private val SPEED: Float, z: Float, visibility: Boolean) : Shape() {
-    override val isOverlapMethodLevel: Double = 0.0
+    override val isOverlapMethodLevel: Double = 1.0
     override var componentShapes: Array<Shape> = arrayOf(RegularPolygonalShape(4, // four seems like a sensible number
             centerX, centerY, 16f / 720f, 1f, 1f, 1f, brightness, z, visibility))
     var brightness: Float = 0.toFloat()
