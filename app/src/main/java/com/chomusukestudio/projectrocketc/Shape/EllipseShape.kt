637a20d0a760412db74477e91cdefe954104f994
chomosuke <38484873+chomosuke@users.noreply.github.com>
2019-07-11 11:20:12 +1200

    Ellipse vectorized

--- app/src/main/java/com/chomusukestudio/projectrocketc/Shape/EllipseShape.kt
+++ app/src/main/java/com/chomusukestudio/projectrocketc/Shape/EllipseShape.kt
@@ -4,7 +4,7 @@ import java.lang.Math.PI
 import java.lang.Math.cos
 import java.lang.Math.sin
 
-class EllipseShape(centerX: Float, centerY: Float, a: Float, b: Float, color: Color, buildShapeAttr: BuildShapeAttr) : Shape() {
+class EllipseShape(center: Vector, a: Float, b: Float, color: Color, buildShapeAttr: BuildShapeAttr) : Shape() {
     override val isOverlapMethodLevel: Double = 0.0
     override lateinit var componentShapes: Array<Shape>
     
@@ -16,11 +16,11 @@ class EllipseShape(centerX: Float, centerY: Float, a: Float, b: Float, color: Co
     
         // generate components triangularShape for EllipseShape isInUse center and a and b
         for (i in 1 until numberOfEdges - 1)
-            componentShapes[i - 1] = TriangularShape(Vector(centerX, centerY + b),
-                    Vector(centerX + a * sin(2.0 * PI * i.toDouble() / numberOfEdges).toFloat(),
-                            centerY + b * cos(2.0 * PI * i.toDouble() / numberOfEdges).toFloat()),
-                    Vector(centerX + a * sin(2.0 * PI * (i + 1).toDouble() / numberOfEdges).toFloat(),
-                            centerY + b * cos(2.0 * PI * (i + 1).toDouble() / numberOfEdges).toFloat()),
+            componentShapes[i - 1] = TriangularShape(Vector(center.x, center.y + b),
+                    Vector(center.x + a * sin(2.0 * PI * i.toDouble() / numberOfEdges).toFloat(),
+                            center.y + b * cos(2.0 * PI * i.toDouble() / numberOfEdges).toFloat()),
+                    Vector(center.x + a * sin(2.0 * PI * (i + 1).toDouble() / numberOfEdges).toFloat(),
+                            center.y + b * cos(2.0 * PI * (i + 1).toDouble() / numberOfEdges).toFloat()),
                     color, buildShapeAttr) // close for modification
     
         this.componentShapes = componentShapes as Array<Shape>
