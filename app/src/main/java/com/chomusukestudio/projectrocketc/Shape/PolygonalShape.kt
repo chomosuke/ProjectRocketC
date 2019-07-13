f045d58545f84475765509d811c19955edda5a28
chomosuke <38484873+chomosuke@users.noreply.github.com>
2019-07-14 00:28:08 +1200

    EarClipPolygonalShape and fun earClip

--- app/src/main/java/com/chomusukestudio/projectrocketc/Shape/PolygonalShape.kt
+++ app/src/main/java/com/chomusukestudio/projectrocketc/Shape/PolygonalShape.kt
@@ -1,5 +1,9 @@
 package com.chomusukestudio.projectrocketc.Shape
 
+import com.chomusukestudio.projectrocketc.randFloat
+import kotlin.collections.ArrayList
+import kotlin.math.PI
+
 class PolygonalShape(vertexes: Array<Vector>, color: Color, buildShapeAttr: BuildShapeAttr): Shape() {
 	override var componentShapes = Array<Shape>(vertexes.size - 2) {
 		TriangularShape(vertexes[0], vertexes[it + 1], vertexes[it + 2], color, buildShapeAttr)
@@ -12,4 +16,64 @@ class PolygonalShape(vertexes: Array<Vector>, color: Color, buildShapeAttr: Buil
 			else -> (componentShapes[index - 2] as TriangularShape).vertex3
 		}
 	}
+}
+
+class EarClipPolygonalShape(private val vertexes: Array<Vector>, color: Color, buildShapeAttr: BuildShapeAttr): Shape() {
+	override var componentShapes = run {
+		val vertexeses = earClip(vertexes)
+		Array<Shape>(vertexeses.size) {
+			TriangularShape(vertexeses[it][0], vertexeses[it][1], vertexeses[it][2], color, buildShapeAttr)
+		}
+	}
+	
+	override fun moveShape(displacement: Vector) {
+		super.moveShape(displacement)
+		totalDisplacement += displacement
+	}
+	private var totalDisplacement = Vector(0f, 0f)
+	fun getVertex(index: Int): Vector {
+		return vertexes[index] + totalDisplacement
+	}
+}
+
+// Triangulate a polygon with Ear clipping method
+fun earClip(vertexes: Array<Vector>): Array<Array<Vector>> {
+	val trianglesVertexes = arrayOfNulls<Array<Vector>>(vertexes.size - 2)
+	var iT = 0
+	val vertexes = vertexes.toCollection(ArrayList())
+	while (vertexes.size > 2) {
+		var iV = 1
+		
+		// find an inner angle smaller than 180 degree
+		while (true) {
+			
+			// if IndexOutOfBounds means wrong order of vertexes
+			val angle1 = (vertexes[iV] - vertexes[iV - 1]).direction
+			val angle2 = (vertexes[iV + 1] - vertexes[iV]).direction
+			if ((angle1 > 0 && (angle1 >= angle2 && angle1-PI <= angle2)) ||
+					(angle1 <= 0 && (angle1 >= angle2 || angle1+PI <= angle2))) {
+				// angle vertexes[i-1]vertexes[i]vertexes[i+1] anti clockwise is smaller than 180 degree
+				// if made into a triangle it won't be outside the polygon
+				
+				val otherVertexes = vertexes.clone() as ArrayList<Vector>
+				otherVertexes.removeAll(arrayOf(vertexes[iV-1], vertexes[iV], vertexes[iV+1]))
+				if (!contain(TriangularOverlapper(vertexes[iV-1], vertexes[iV], vertexes[iV+1]), otherVertexes)) {
+					// triangle also doesn't contain any other vertexes
+					break
+				}
+			}
+			iV++ // see if the next triangle meets requirement
+		}
+		
+		// ear can be clipped
+		trianglesVertexes[iT++] = arrayOf(vertexes[iV-1], vertexes[iV], vertexes[iV+1])
+		vertexes.removeAt(iV)
+	}
+	return trianglesVertexes as Array<Array<Vector>>
+}
+private fun contain(triangularOverlapper: TriangularOverlapper, points: Collection<Vector>): Boolean {
+	for (point in points)
+		if (triangularOverlapper overlap PointOverlapper(point))
+			return true
+	return false
 }
