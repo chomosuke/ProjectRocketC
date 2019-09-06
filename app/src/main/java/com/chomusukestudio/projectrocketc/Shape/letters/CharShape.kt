128cc4dbf203046761176239c50286819b0af689
chomosuke <38484873+chomosuke@users.noreply.github.com>
2019-09-06 15:16:06 +1000

    CharShape unfinished and hopefully will never finish

---
+++ app/src/main/java/com/chomusukestudio/projectrocketc/Shape/letters/CharShape.kt
@@ -0,0 +1,12 @@
+package com.chomusukestudio.projectrocketc.Shape.letters
+
+import com.chomusukestudio.projectrocketc.Shape.*
+
+//class CharShape(char: Char, center: Vector, size: Vector, color: Color, buildShapeAttr: BuildShapeAttr) : Shape() {
+//    override var componentShapes = run {
+//        val componentShapes = when(char) {
+//            "A" -> arrayOf()
+//        }
+//    }
+//    override val overlapper = EmptyOverlapper()
+//}
